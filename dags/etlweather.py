cc01f083ec87be53ea2c187cd3d02ae6153f6368
Hashi-corp <harishkrishnan.p2022@vitstudent.ac.in>
2025-07-04 12:35:02 +0530

    created etlweather.py

---
+++ dags/etlweather.py
@@ -0,0 +1,5 @@
+from airflow import DAG
+from airflow.providers.http.hooks.http import HttpHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.decorators import task
+from airflow.utils.dates import days_ago
